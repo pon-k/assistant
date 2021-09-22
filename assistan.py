3fe3757fe1ef5345dc94c307b2b46f6c6c41b8e7
pon-k <ignoretsu@gmail.com>
2021-09-22 21:05:59 +0100

    three

--- assistan.py
+++ assistan.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from PIL import ImageTk, Image
 from random import randint
 import wikipedia
+from datetime import datetime
+from pyowm.owm import OWM
 
 
 class App:
@@ -13,7 +15,7 @@ class App:
         self.label = tk.Label(master, image=self.ph)
         self.label.image = self.ph
         self.entry = tk.Entry(master, width=70)
-        self.response = tk.Text(master, width=60, height=5, fg='white', bg ='black')
+        self.response = tk.Text(master, width=60, height=5, fg='white', bg='black')
         self.response.pack(side=tk.BOTTOM)
         self.response.insert('1.0', 'What do you need?')
         self.response.config(state='disabled')
@@ -27,6 +29,7 @@ class App:
         self.entry.delete(0, tk.END)
         self.response.config(state='normal')
         self.response.delete('1.0', tk.END)
+
         if 'wikipedia' in qtn:
             topic = qtn.replace('wikipedia', '')
             try:
@@ -34,7 +37,27 @@ class App:
                 self.response.insert('1.0', f'{summary}')
             except (wikipedia.exceptions.DisambiguationError, wikipedia.exceptions.PageError):
                 self.response.insert('1.0', "It's either not there or you need to be more specific.")
+
+        elif 'day' or 'date' or 'time' in qtn:
+            crr_dt = datetime.now()
+            message = crr_dt.strftime('It\'s currently %A, %d %B, %Y. The time is %I:%M %p.')
+            self.response.insert('1.0', message)
+
+        elif 'temp' or 'weather' in qtn:
+            owm = OWM('52dc819def4984767efeb4210ac563a7')
+            wmgr = owm.weather_manager()
+            loc = wmgr.weather_at_place('London,GB')
+            wea = loc.weather
+            temp = wea.temperature('celsius')
+            self.response.insert('1.0', f'The weather is {wea}. The minimum temperature today is {temp["temp_min"]}, '
+                                        f'and the max temperature is {temp["temp_max"]}.')
+
         self.response.config(state='disabled')
+
+
+
+
+
 
     def changeimg(self, master):
         image_list = ['bbook.png', 'bbird.png', 'bfood.png', 'bpass.png']
